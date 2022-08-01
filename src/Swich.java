79fc2b268df0321f722b11354f1e524e90df27de
vasasandul <vasean555@icloud.com>
2022-08-01 11:59:33 +0300

    homework swich

---
+++ src/Swich.java
@@ -0,0 +1,28 @@
+public class Swich {
+
+        public static void main(String[] args) {
+
+            int a = 1;
+
+            switch (a) {
+                case 1:
+                    System.out.println("вы ввели один");
+                    break;
+                case 2:
+                    System.out.println("вы ввели два");
+                    break;
+                case 3:
+                    System.out.println("вы ввели три");
+                    break;
+                case 4:
+                    System.out.println("вы ввели четыре");
+                    break;
+                case 5:
+                    System.out.println("вы ввели пять");
+                    break;
+                default:
+                    System.out.println("неверный ввод числа");
+            }
+        }
+    }
+
