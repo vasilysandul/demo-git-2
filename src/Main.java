3780ceb4a104da4b6c8444e505630129f91d1af7
vasasandul <vasean555@icloud.com>
2022-08-01 11:56:33 +0300

    homework 3

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,15 @@
 public class Main {
+    public static void main(String[] args) {
+
+        int a = 3;
+        int b = 3;
+
+        if (a > b) {
+            System.out.println("A > B");
+        } else if (b > a) {
+            System.out.println("B > A");
+        } else {
+            System.out.println("A == B");
+        }
+    }
 }
