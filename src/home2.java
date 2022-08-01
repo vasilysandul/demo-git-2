bef77177a28cd06217e5f4838679ace79ebc5a4e
vasasandul <vasean555@icloud.com>
2022-08-01 12:09:48 +0300

    home 2

---
+++ src/home2.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    static void printSome() {
+
+        int a = 20;
+        int b = 3;
+
+        System.out.println(a * b - 3);
+    }
+
+    public static void main(String[] args) {
+        printSome();
+    }
+}
+
