8a0687774ce5e9c1829e3ad6211367c2437fa5bd
vasasandul <vasean555@icloud.com>
2022-08-01 12:07:26 +0300

    home1

---
+++ src/homework.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 10000;
+        short b = 1000;
+        byte c = 100;
+
+        System.out.println("a + b + c = " + (a + b + c));
+        System.out.println("a - b - c = " + (a - b - c));
+        System.out.println("a * b * c = " + (a * b * c));
+        System.out.println("a / b / c = " + (a / b / c));
+    }
+}
